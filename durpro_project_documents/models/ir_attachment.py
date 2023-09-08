ce89d38ca1c8cdff65609f070fdafb33b0a66e09
Marc Durepos <marc@bemade.org>
2023-09-08 08:24:09 -0400

    durpro_project_documents: added action for sending signature request to list view.

--- durpro_project_documents/models/ir_attachment.py
+++ durpro_project_documents/models/ir_attachment.py
@@ -1,5 +1,5 @@
-from odoo import models, fields, api
-
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
@@ -7,5 +7,7 @@ class IrAttachment(models.Model):
     document_ids = fields.One2many('documents.document', 'attachment_id')
 
     def send_for_signature(self):
+        if self.filtered(lambda r: r.mimetype != 'application/pdf'):
+            raise UserError(_('Only PDF files can be sent for signature.'))
         rule = self.env.ref('documents_sign.documents_sign_rule_sign_directly')
         return rule.create_record(documents=self.mapped('document_ids'))

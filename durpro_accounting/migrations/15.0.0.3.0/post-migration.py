08c474fe066e1c47a87ee817349c712a025db62f
Marc Durepos <marc@bemade.org>
2023-12-28 10:36:10 -0500

    fix durpro_accounting migration script

--- durpro_accounting/migrations/15.0.0.3.0/post-migration.py
+++ durpro_accounting/migrations/15.0.0.3.0/post-migration.py
@@ -1,8 +1,9 @@
 from odoo import api, SUPERUSER_ID
 
 
-def migrate(env, version):
+def migrate(cr, version):
     """ Uninstall the durpro_15_16_accounting_fix module after running it"""
+    env = api.Environment(cr, SUPERUSER_ID, {})
     env['ir.module.module'].search(
         [('name', '=', 'durpro_15_16_accounting_fix')]
     ).button_immediate_uninstall()
--- durpro_accounting/migrations/15.0.0.3.0/pre-migration.py
+++ durpro_accounting/migrations/15.0.0.3.0/pre-migration.py
@@ -1,10 +1,12 @@
 from openupgradelib import openupgrade
-from odoo import SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
-def migrate(env, version):
+def migrate(cr, version):
     """ Install the durpro_15_16_accounting_fix module within a module
     upgrade so that Odoo.sh will not have a time limit for it. """
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
     env['ir.module.module'].search(
         [('name', '=', 'durpro_15_16_accounting_fix')]
     ).button_immediate_install()

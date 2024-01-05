812298dcdf83c77acc8998dd562dedeb0c5421da
Marc Durepos <marc@bemade.org>
2024-01-05 13:52:03 -0500

    add sudo() to ir.config.parameter read.

--- durpro_sale/__manifest__.py
+++ durpro_sale/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Durpro Sale",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.3",
     "license": "Other proprietary",
     "author": "Durpro Ltd",
     "category": "Generic Modules",

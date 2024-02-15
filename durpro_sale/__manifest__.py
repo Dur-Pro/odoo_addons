15009bc558630f62708bccdd5b08a788e2a70591
Benoît Vézina <benoit@vezina.biz>
2024-02-15 14:11:45 -0500

    to prod

--- durpro_sale/__manifest__.py
+++ durpro_sale/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Durpro Sale",
-    "version": "15.0.1.2.2",
+    "version": "15.0.1.2.3",
     "license": "Other proprietary",
     "author": "Durpro Ltd",
     "category": "Generic Modules",

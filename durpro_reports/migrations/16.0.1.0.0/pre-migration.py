8f7a86780161bba5f1c1012aba76c237d2270f30
Marc Durepos <marc@bemade.org>
2024-02-27 14:18:05 -0500

    remove old durpro_reports views on migration to 16.0

--- durpro_reports/migrations/16.0.1.0.0/pre-migration.py
+++ durpro_reports/migrations/16.0.1.0.0/pre-migration.py
@@ -1,5 +1,5 @@
 def migrate(cr, version):
     cr.execute("""
     WITH reports_views as (SELECT res_id FROM ir_model_data WHERE model='ir.ui.view' and module='durpro_reports')
-    DELETE FROM ir_ui_view WHERE id in reports_views OR inherit_id in report_views
+    DELETE FROM ir_ui_view WHERE id in (select * from reports_views) OR inherit_id in (select * from reports_views)
     """)

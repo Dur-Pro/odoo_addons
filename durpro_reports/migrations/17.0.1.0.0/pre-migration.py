2dd17c55250e1a4bf2bf72907c21a7600f522cd1
Marc Durepos <marc@bemade.org>
2024-03-21 17:29:02 -0400

    durpro_reports migrate script

--- durpro_reports/migrations/17.0.1.0.0/pre-migration.py
+++ durpro_reports/migrations/17.0.1.0.0/pre-migration.py
@@ -3,11 +3,5 @@ from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
     cr.execute("""
-    DELETE from ir_ui_view WHERE 
-        inherit_id IN (
-            SELECT id from ir_model_data WHERE model='ir.ui.view' and module='durpro_reports'
-            )
-        OR
-        id in (
-            SELECT id from ir_model_data WHERE model='ir.ui.view' and module='durpro_reports'
-            )""")
+    DELETE FROM ir_ui_view where id=1040 
+    """)

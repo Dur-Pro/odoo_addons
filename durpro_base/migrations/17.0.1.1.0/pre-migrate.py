3038eda1723694aee220f55321e239b818ba3d35
Marc Durepos <marc@bemade.org>
2024-04-04 15:21:39 -0400

    durpro_base: add a pre-migration script to remove unwanted modules after migration to 17

---
+++ durpro_base/migrations/17.0.1.1.0/pre-migrate.py
@@ -0,0 +1,36 @@
+from odoo import api, SUPERUSER_ID
+
+def migrate(cr, version):
+    modules_to_uninstall = [
+        'account_payment_term_discount',
+        'amount_residual_signed_fix',
+        'bemade_multiple_billing_contacts',
+        'bemade_odoo_partner_scrapper',
+        'bemade_odoo_partner_scrapper_js_only',
+        'bemade_open_project_details',
+        'bemade_user_custom_apps_order',
+        'crm_phonecall',
+        'discuss_search_view_cr',
+        'document_knowledge',
+        'document_page',
+        'hr_attendance_fix',
+        'mail_attach_existing_attachment',
+        'mail_show_follower',
+        'menu_sequence_per_user',
+        'mrp_production_serial_matrix',
+        'owncloud_odoo',
+        'project_duplicate_subtask',
+        'project_task_default_stage',
+        'prt_mail_messages',
+        'purchase_delivery_split_date',
+        'purchase_location_by_line',
+        'zb_alias_name',
+    ]
+
+    sql = """
+    UPDATE ir_module_module
+    SET state='to remove'
+    WHERE name in %s and state='installed'
+    """
+
+    cr.execute(sql, (tuple(modules_to_uninstall)))

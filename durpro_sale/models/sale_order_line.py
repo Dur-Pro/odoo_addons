6f4a1d1f823761635768e57de03c60397d12f5c0
Marc Durepos <marc@bemade.org>
2024-05-13 16:43:18 -0400

    durpro_sale: improve line numbering behaviour.

--- durpro_sale/models/sale_order_line.py
+++ durpro_sale/models/sale_order_line.py
@@ -29,7 +29,9 @@ class SaleOrderLine(models.Model):
         if is_dropship and purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
             qty = 0.0
             for po_line in purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
+                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom,
+                                                             rounding_method='HALF-UP')
             return qty
         else:
-            return super(sale.SaleOrderLine, self)._get_qty_procurement(previous_product_uom_qty=previous_product_uom_qty)
+            return super(sale.SaleOrderLine, self)._get_qty_procurement(
+                previous_product_uom_qty=previous_product_uom_qty)

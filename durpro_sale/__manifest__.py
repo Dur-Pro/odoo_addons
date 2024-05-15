97bfd3e463d74da304a20180e716f9bfde32a859
Marc Durepos <marc@bemade.org>
2024-05-15 08:59:33 -0400

    durpro_sale: remove sequence_no from report

--- durpro_sale/__manifest__.py
+++ durpro_sale/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Durpro Sale",
-    "version": "15.0.1.2.5",
+    "version": "15.0.1.2.6",
     "license": "Other proprietary",
     "author": "Durpro Ltd",
     "category": "Generic Modules",

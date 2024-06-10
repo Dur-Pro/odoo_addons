8dad46c40a0be1fd2ee8a1f5d2f4b9616bf784dc
Marc Durepos <marc@bemade.org>
2024-06-10 12:33:59 -0400

    remove old code on action send for sales orders (durpro-sale)

--- durpro_sale/models/sale_order.py
+++ durpro_sale/models/sale_order.py
@@ -26,36 +26,6 @@ class SaleOrder(models.Model):
         if self.env.ref('sale.menu_sale_order', raise_if_not_found=False):
             self.env.ref('sale.menu_sale_order').unlink()
 
-    def action_sale_order_send(self):
-        ''' Opens a wizard to compose an email, with relevant mail template loaded by default '''
-        self.ensure_one()
-        template_id = self._find_mail_template()
-        lang = self.env.context.get('lang')
-        template = self.env['mail.template'].browse(template_id)
-        if template.lang:
-            lang = template._render_lang(self.ids)[self.id]
-        ctx = {
-            'default_model': 'sale.order',
-            'default_res_ids': [self.ids[0]],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
-            'default_composition_mode': 'comment',
-            'mark_so_as_sent': True,
-            'custom_layout': "mail.mail_notification_paynow",
-            'proforma': self.env.context.get('proforma', False),
-            'force_email': True,
-            'model_description': self.with_context(lang=lang).type_name,
-        }
-        return {
-            'type': 'ir.actions.act_window',
-            'view_mode': 'form',
-            'res_model': 'mail.compose.message',
-            'views': [(False, 'form')],
-            'view_id': False,
-            'target': 'new',
-            'context': ctx,
-        }
-
     def action_confirm(self):
         ref_required = self.env['ir.config_parameter'].sudo().get_param(
             'durpro_sale.require_sale_reference',

adc88f839a8100a8d704f4ecbf81d7643c650b85
Marc Durepos <marc@bemade.org>
2024-06-20 14:21:31 -0400

    fix broken super() call in durpro_current_user_as_invoice_user

--- durpro_current_user_as_invoice_user/wizard/sale_make_invoice_advance.py
+++ durpro_current_user_as_invoice_user/wizard/sale_make_invoice_advance.py
@@ -2,12 +2,14 @@ from odoo import models
 
 
 class SaleMakeInvoiceAdvance(models.TransientModel):
-    _inherit = 'sale.advance.payment.inv'
+    _inherit = "sale.advance.payment.inv"
 
-    def _prepare_invoice_values(self, order, name, amount, so_line):
-        invoice_vals = super()._prepare_invoice_values(order, name, amount, so_line)
-        invoice_vals.update({
-            'invoice_user_id': self.env.user.id,
-            'user_id': self.env.user.id,
-        })
+    def _prepare_invoice_values(self, order, so_line):
+        invoice_vals = super()._prepare_invoice_values(order, so_line)
+        invoice_vals.update(
+            {
+                "invoice_user_id": self.env.user.id,
+                "user_id": self.env.user.id,
+            }
+        )
         return invoice_vals

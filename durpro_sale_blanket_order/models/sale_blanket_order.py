ebfc7c3266ea6fc756ab5defa29c6660d0e623fa
Marc Durepos <marc@bemade.org>
2024-08-06 12:00:51 -0400

    [IMP] durpro_sale_blanket_order: note as HTML with correct default
    Make the note field on blanket orders an HTML field instead of text, and
    have it set its default exactly the same way as sale.order sets its
    default note.

--- durpro_sale_blanket_order/models/sale_blanket_order.py
+++ durpro_sale_blanket_order/models/sale_blanket_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from odoo.tools import html_keep_url, is_html_empty
 
 
 class SaleBlanketOrder(models.Model):
@@ -15,3 +16,36 @@ class SaleBlanketOrder(models.Model):
         comodel_name="crm.tag",
         string="Tags",
     )
+
+    # Make note an HTML field like on sales orders and get it the same default
+    # Partner-based computes
+    note = fields.Html(
+        string="Terms and conditions",
+        compute="_compute_note",
+        store=True,
+        readonly=False,
+        precompute=True,
+    )
+    terms_type = fields.Selection(related="company_id.terms_type")
+
+    @api.depends("partner_id")
+    def _compute_note(self):
+        """Direct copy/paste from sale.order"""
+        use_invoice_terms = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("account.use_invoice_terms")
+        )
+        if not use_invoice_terms:
+            return
+        for order in self:
+            order = order.with_company(order.company_id)
+            if order.terms_type == "html" and self.env.company.invoice_terms_html:
+                baseurl = html_keep_url(self.env.company.get_base_url() + "/terms")
+                context = {"lang": order.partner_id.lang or self.env.user.lang}
+                order.note = _("Terms & Conditions: %s", baseurl)
+                del context
+            elif not is_html_empty(self.env.company.invoice_terms):
+                order.note = order.with_context(
+                    lang=order.partner_id.lang
+                ).env.company.invoice_terms
